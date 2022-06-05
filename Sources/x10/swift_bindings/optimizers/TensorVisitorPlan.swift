af0e7075fe0a95ab97a14938b85b4c3053c77979
Philip Turner <philipturner.AR@gmail.com>
2022-06-05 19:28:00 -0400

    Update TensorVisitorPlan.swift

--- Sources/x10/swift_bindings/optimizers/TensorVisitorPlan.swift
+++ Sources/x10/swift_bindings/optimizers/TensorVisitorPlan.swift
@@ -12,8 +12,8 @@
 // // See the License for the specific language governing permissions and
 // // limitations under the License.
 
-// import _Differentiation
-// import TensorFlow
+import _Differentiation
+import TensorFlow
 
 // typealias GradMapFn = (inout Tensor<Float>, Tensor<Float>, Int) -> Void
 // // A partially type erased base-class that essentially provides a
