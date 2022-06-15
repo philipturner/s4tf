06e5938e7ea07efdda9bed37cc11ca62d21ccafb
Philip Turner <philipturner.AR@gmail.com>
2022-06-15 14:20:56 -0400

    commit progress

--- Sources/TensorFlow/Layers/Normalization.swift
+++ Sources/TensorFlow/Layers/Normalization.swift
@@ -14,123 +14,45 @@
 
 import _Differentiation
 
-@differentiable(reverse, wrt: (input, mean, variance, offset, scale))
-private func normalize<Scalar: TensorFlowFloatingPoint>(
-  _ input: Tensor<Scalar>,
-  mean: Tensor<Scalar>,
-  variance: Tensor<Scalar>,
-  offset: Tensor<Scalar>,
-  scale: Tensor<Scalar>,
-  varianceEpsilon: Tensor<Scalar>
-) -> Tensor<Scalar> {
-  return input + mean + variance + offset + scale
-}
+//@differentiable(reverse, wrt: (input, mean, variance, offset, scale))
+//private func normalize<Scalar: TensorFlowFloatingPoint>(
+//  _ input: Tensor<Scalar>,
+//  mean: Tensor<Scalar>,
+//  variance: Tensor<Scalar>,
+//  offset: Tensor<Scalar>,
+//  scale: Tensor<Scalar>,
+//  varianceEpsilon: Tensor<Scalar>
+//) -> Tensor<Scalar> {
+//  return input + mean + variance + offset + scale
+//}
 
 public struct BatchNorm<Scalar: TensorFlowFloatingPoint>: Layer {
-  @noDerivative public let axis: Int
-  @noDerivative public let momentum: Scalar
   public var offset: Tensor<Scalar>
   public var scale: Tensor<Scalar>
-  @noDerivative public let epsilon: Scalar
-  @noDerivative public var runningMean: Parameter<Scalar>
-  @noDerivative public var runningVariance: Parameter<Scalar>
 
   @differentiable(reverse)
   public func callAsFunction(_ input: Tensor<Scalar>) -> Tensor<Scalar> {
-    let inputRank = input.rank
-    let positiveAxis = (inputRank + axis) % inputRank
-    let offsetOriginal = self.offset
-    let scaleOriginal = self.scale
-    let (offset, scale) = Self._sr13263workaround(offset: offsetOriginal,
-                                                  scale: scaleOriginal,
-                                                  input: input,
-                                                  positiveAxis: positiveAxis)
-    switch Context.local.learningPhase {
-    case .training:
-      return doTraining(input, offset: offset, scale: scale, axis: positiveAxis)
-    case .inference:
-      return doInference(input, offset: offset, scale: scale)
-    }
-  }
-  
-  @inline(never)
-  @differentiable(reverse) // if the function is `public` or `internal`, the compiler crashes
-  private static func _sr13263workaround(
-    offset: Tensor<Scalar>,
-    scale: Tensor<Scalar>,
-    input: Tensor<Scalar>,
-    positiveAxis: Int
-  ) -> (Tensor<Scalar>, Tensor<Scalar>) {
-    if positiveAxis != input.rank - 1 {
-      var broadcastShape = TensorShape([Int](repeating: 1, count: input.rank))
-      broadcastShape[positiveAxis] = input.shape[positiveAxis]
-      return (offset.reshaped(to: broadcastShape), scale.reshaped(to: broadcastShape))
+    if true {
+      let eps = withoutDerivative(at: input) { Tensor(2.333, deviceAndPrecisionLike: $0) }
+      return eps
     } else {
-      return (offset, scale)
+      return doInference(input)
     }
   }
   
-  private func doTraining(
-    _ input: Tensor<Scalar>, offset: Tensor<Scalar>, scale: Tensor<Scalar>, axis: Int
-  ) -> Tensor<Scalar> {
-//    return input
-    var normalizedAxes = Array(0..<input.rank)
-    normalizedAxes.remove(at: axis)
-    let moments = input.moments(alongAxes: normalizedAxes)
-    let decayMomentum = Tensor(1 - momentum)//, on: input.device)
-    let isReducedPrecision = withoutDerivative(at: input) { $0.isReducedPrecision }
-    var momentsMean = moments.mean
-    var momentsVariance = moments.variance
-    if isReducedPrecision {
-      momentsMean = momentsMean.toFullPrecision
-      momentsVariance = momentsVariance.toFullPrecision
-    }
-    runningMean.value += (momentsMean - runningMean.value) * decayMomentum
-    runningVariance.value += (momentsVariance - runningVariance.value) * decayMomentum
-    let eps = withoutDerivative(at: input) { Tensor(epsilon, deviceAndPrecisionLike: $0) }
-    return eps
-//    return normalize(
-//      input,
-//      mean: moments.mean, variance: moments.variance,
-//      offset: offset, scale: scale,
-//      varianceEpsilon: eps)
-  }
-
   private func doInference(
-    _ input: Tensor<Scalar>, offset: Tensor<Scalar>, scale: Tensor<Scalar>
+    _ input: Tensor<Scalar>
   ) -> Tensor<Scalar> {
-    let isReducedPrecision = withoutDerivative(at: input) { $0.isReducedPrecision }
-    let runningVarianceValue =
-      isReducedPrecision ? runningVariance.value.toReducedPrecision : runningVariance.value
-    let runningMeanValue =
-      isReducedPrecision ? runningMean.value.toReducedPrecision : runningMean.value
-    let eps = withoutDerivative(at: input) { Tensor(epsilon, deviceAndPrecisionLike: $0) }
+    let eps = withoutDerivative(at: input) { Tensor(2.333, deviceAndPrecisionLike: $0) }
     return eps
-//    return normalize(
-//      input,
-//      mean: runningMeanValue, variance: runningVarianceValue,
-//      offset: offset, scale: scale,
-//      varianceEpsilon: eps)
   }
 }
 
 public struct LayerNorm<Scalar: TensorFlowFloatingPoint> {
-  public var offset: Tensor<Scalar>
-  public var scale: Tensor<Scalar>
-  @noDerivative public let axis: Int
-  @noDerivative public let epsilon: Scalar
-
   @differentiable(reverse)
   public func callAsFunction(_ input: Tensor<Scalar>) -> Tensor<Scalar> {
-    let epsilon = withoutDerivative(at: input) { Tensor(self.epsilon, deviceAndPrecisionLike: $0) }
-    let positiveAxis = (input.rank + axis) % input.rank
-    
-    var broadcastShape = TensorShape(Array(repeating: 1, count: input.rank))
-    broadcastShape[positiveAxis] = input.shape[positiveAxis]
-    let offset = self.offset.reshaped(to: broadcastShape)
-    let scale = self.scale.reshaped(to: broadcastShape)
-    let moments = input.moments(alongAxes: positiveAxis)
-    let inv = rsqrt(moments.variance)
+    let moments = input.moments(alongAxes: 1)
+    let inv = rsqrt(moments.mean)
     return moments.mean * inv
   }
 }
